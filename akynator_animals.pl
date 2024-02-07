00b8014cb75982fe370fea71bce2555c6b4396fd
MariaPavlog <pavlogradskaam@gmail.com>
2024-02-08 01:16:15 +0300

    akynator animals. the fact base

---
+++ akynator_animals.pl
@@ -0,0 +1,227 @@
+%1.coelenterates
+%2. worms
+%3. arthropods
+%4.fish
+%5. reptiles
+%6.birds
+%7. Mammals
+type(cat,7).
+type(chicken,6).
+type(cow, 7).
+type(horse,7).
+type(snake,5).
+type(carp,4).
+type(butterfly, 3).
+type(spider, 3).
+type(mole, 7).
+type(sparrow, 6).
+type(crocodile, 5).
+type(lion,7).
+type(coral,1).
+type(kangaroo,7).
+type(monkey,7).
+type(ostrich, 6).
+type(jellyfish,1).
+type(eatthworm, 2).
+type(leech,2).
+type(bee,3).
+type(shark,4).
+type(turtle,5).
+type(penguin,6).
+type(polar_bear, 7).
+type(wolf,7).
+type(hare,7).
+type(whale,7).
+
+
+%living environment
+%1.air
+%2.in the ground
+%3.on the surface of the earth
+%4. in water
+%5. in water, but you need air
+
+environment(cat,3).
+environment(chicken,3).
+environment(cow, 3).
+environment(horse,3).
+environment(snake,3).
+environment(carp,4).
+environment(butterfly, 1).
+environment(spider, 3).
+environment(mole, 2).
+environment(sparrow, 1).
+environment(crocodile, 5).
+environment(lion,3).
+environment(coral,4).
+environment(kangaroo,3).
+environment(monkey,3).
+environment(ostrich, 3).
+environment(jellyfish,4).
+environment(eatthworm, 2).
+environment(leech,4).
+environment(bee,1).
+environment(shark,4).
+environment(turtle,5).
+environment(penguin,3).
+environment(polar_bear, 3).
+environment(wolf,3).
+environment(hare,3).
+environment(whale,4).
+
+
+%climate zone
+%1. tropical
+%2. temperate
+%3. polar
+%4. temperate + tropical
+zone(cat,4).
+zone(chicken,2).
+zone(cow, 4).
+zone(horse,4).
+zone(snake,4).
+zone(carp,4).
+zone(butterfly, 4).
+zone(spider, 4).
+zone(mole, 4).
+zone(sparrow, 2).
+zone(crocodile, 1).
+zone(lion,1).
+zone(coral,1).
+zone(kangaroo,1).
+zone(monkey,1).
+zone(ostrich, 1).
+zone(jellyfish,4).
+zone(eatthworm, 4).
+zone(leech,4).
+zone(bee,4).
+zone(shark,1).
+zone(turtle,4).
+zone(penguin,3).
+zone(polar_bear, 3).
+zone(wolf,2).
+zone(hare,2).
+zone(whale,4).
+
+
+%agricultural industry
+agrar(cat,0).
+agrar(chicken,1).
+agrar(cow, 1).
+agrar(horse,1).
+agrar(snake,0).
+agrar(carp,1).
+agrar(butterfly, 0).
+agrar(spider, 0).
+agrar(mole, 0).
+agrar(sparrow, 0).
+agrar(crocodile, 0).
+agrar(lion,0).
+agrar(coral,0).
+agrar(kangaroo,0).
+agrar(monkey,0).
+agrar(ostrich, 0).
+agrar(jellyfish,0).
+agrar(eatthworm, 1).
+agrar(leech,0).
+agrar(bee,1).
+agrar(shark,0).
+agrar(turtle,0).
+agrar(penguin,0).
+agrar(polar_bear, 0).
+agrar(wolf,0).
+agrar(hare,0).
+agrar(whale,0).
+
+
+
+pet(cat,1).
+pet(chicken,0).
+pet(cow, 0).
+pet(horse,0).
+pet(snake,1).
+pet(carp,0).
+pet(butterfly, 0).
+pet(spider, 1).
+pet(mole, 0).
+pet(sparrow, 0).
+pet(crocodile, 0).
+pet(lion,0).
+pet(coral,0).
+pet(kangaroo,0).
+pet(monkey,1).
+pet(ostrich, 0).
+pet(jellyfish,0).
+pet(eatthworm, 0).
+pet(leech,0).
+pet(bee,0).
+pet(shark,0).
+pet(turtle,1).
+pet(penguin,0).
+pet(polar_bear, 0).
+pet(wolf,0).
+pet(hare,0).
+pet(whale,0).
+
+
+danger(cat,0).
+danger(chicken,0).
+danger(cow, 0).
+danger(horse,0).
+danger(snake,1).
+danger(carp,0).
+danger(butterfly, 0).
+danger(spider, 1).
+danger(mole, 0).
+danger(sparrow, 0).
+danger(crocodile, 1).
+danger(lion,1).
+danger(coral,0).
+danger(kangaroo,0).
+danger(monkey,0).
+danger(ostrich, 0).
+danger(jellyfish,1).
+danger(eatthworm, 0).
+danger(leech,0).
+danger(bee,0).
+danger(shark,1).
+danger(turtle,0).
+danger(penguin,0).
+danger(polar_bear, 1).
+danger(wolf,1).
+danger(hare,0).
+danger(whale,0).
+
+
+%eat
+%1.food of animal origin
+%2. of plant origin
+%3. omnivorous
+
+eat(cat,1).
+eat(chicken,2).
+eat(cow, 2).
+eat(horse,2).
+eat(snake,1).
+eat(carp,3).
+eat(butterfly, 2).
+eat(spider, 1).
+eat(mole, 1).
+eat(sparrow, 2).
+eat(crocodile, 1).
+eat(lion,1).
+eat(coral,1).
+eat(kangaroo,2).
+eat(monkey,3).
+eat(ostrich, 2).
+eat(jellyfish,1).
+eat(eatthworm, 2).
+eat(leech,1).
+eat(bee,2).
+eat(shark,1).
+eat(turtle,1).
+eat(penguin,1).
+eat(polar_bear, 1).
+eat(wolf,1).
+eat(hare,2).
+eat(whale,1).
