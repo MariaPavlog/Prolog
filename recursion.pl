88d3cab09ec897698124be9daa226ba201c3a7b7
MariaPavlog <pavlogradskaam@gmail.com>
2024-03-29 03:57:14 +0300

    option number 9, task 2, actions with numbers

--- recursion.pl
+++ recursion.pl
@@ -80,3 +80,47 @@ remove_el([H|T], Sum, [H|Res]) :-
     remove_el(T, Sum, Res).
 
 
+%find the minimum number
+%min/3
+%min(+X,+Y,?Z)
+min(X,Y,Y):-X>Y.
+min(X,_,X).
+
+%find the minimum digit of the number, upward recursion
+%find_min/2
+%find_min(+X, -M)
+find_min(0,9).
+find_min(X,M1):- X1 is X //10, Cifr is X mod 10, find_min(X1,M), min(Cifr,M,M1).
+
+%downward recursion
+find_min_d(X, Min):- find_min_d(X, 9, Min).
+find_min_d(0,Min,Min).
+find_min_d(X,Min1,Result):- X1 is X // 10, Cifr is X mod 10,
+    min(Cifr, Min1, NewMin),find_min_d(X1, NewMin, Result).
+
+
+
+
+%the mult of the digits of the number, recursion upwards
+%mult_cifr_up/2
+%mult_cifr_up(+X,?C)
+mult_cifr_up(1,1).
+mult_cifr_up(0,1).
+mult_cifr_up(X,MultCifr):-X>0,X1 is X // 10, Ost is X mod 10,mult_cifr_up(X1, MultCifr1), not(Ost is 5), not(Ost is 0), MultCifr is MultCifr1 * Ost.
+mult_cifr_up(X,M):-X>0,X1 is X // 10, mult_cifr_up(X1, M).
+
+
+%the mult of the digits of the number, recursion downwards
+%mult_cifr_down/2
+%mult_cifr_down(+X,?C)
+mult_cifr_down(N,X):- mult_cifr_down(N,1,X).
+mult_cifr_down(1,X,X):-!.
+mult_cifr_down(N,CurX,X):-N>0,N1 is N // 10, Ost is N mod 10,not(Ost is 5), not(Ost is 0), NewX is CurX * Ost, mult_cifr_down(N1, NewX, X).
+mult_cifr_down(N,CurX,X):-N>0,N1 is N // 10, mult_cifr_down(N1, CurX,X).
+
+%the largest common divisor of two numbers
+%nod/3
+%nod(+A,+B,?C)
+nod(A,0,A):-!.
+nod(_,0,_):-!,fail.
+nod(A,B,C):-Ost is A mod B, nod(B,Ost,C).
